5343e6a61bc54737acf81c768acebd3687302a7b
Roman Yushchenko <roman.yushchenko@akvelon.com>
2017-11-13 09:54:46 +0200

    start project

---
+++ src/main/java/com/romchik/spring/mypractice/storeProduct/StoreProductApplication.java
@@ -0,0 +1,12 @@
+package com.romchik.spring.mypractice.storeProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProductApplication.class, args);
+	}
+}
---
+++ src/test/java/com/romchik/spring/mypractice/storeProduct/StoreProductApplicationTests.java
@@ -0,0 +1,16 @@
+package com.romchik.spring.mypractice.storeProduct;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreProductApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
